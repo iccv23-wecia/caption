e45d29af2f2815299e0fe9294f991953f359bfa7
iccv23-wecia <132705674+iccv23-wecia@users.noreply.github.com>
2023-07-10 18:03:07 +0200

    Update __init__.py

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -34,6 +34,7 @@ def install_local_package(folder_name):
 
 install("evaluate")
 install("nltk")
+install("rouge_score")
 # install("requests==2.25.1")
 
 # install_local_package("package_folder_name")
